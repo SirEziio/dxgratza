903efb0c0fd1c42fa065678955ac15dc127136ca
Daniel Gratza <danielgratza@Daniels-MacBook-Pro.local>
2025-04-03 10:55:26 +0200

    Fix: Remove experimental.appDir from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  output: "standalone", // Helps with Vercel deployment
 };
 
 export default nextConfig;
